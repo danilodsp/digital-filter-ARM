f0dab212cfde5ea9a6698a9a3c6688eea0a77351
Danilo <danilopena.ba@gmail.com>
2018-12-28 10:05:29 -0300

    IIR filter with SOS structure using ADC

--- code/filter/filter/Debug/subdir_vars.mk
+++ code/filter/filter/Debug/subdir_vars.mk
@@ -8,32 +8,26 @@ CMD_SRCS += \
 
 C_SRCS += \
 ../main.c \
-../tm4c123gh6pm_startup_ccs.c \
-C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.c 
+../tm4c123gh6pm_startup_ccs.c 
 
 OBJS += \
 ./main.obj \
-./tm4c123gh6pm_startup_ccs.obj \
-./uartstdio.obj 
+./tm4c123gh6pm_startup_ccs.obj 
 
 C_DEPS += \
 ./main.pp \
-./tm4c123gh6pm_startup_ccs.pp \
-./uartstdio.pp 
+./tm4c123gh6pm_startup_ccs.pp 
 
 C_DEPS__QUOTED += \
 "main.pp" \
-"tm4c123gh6pm_startup_ccs.pp" \
-"uartstdio.pp" 
+"tm4c123gh6pm_startup_ccs.pp" 
 
 OBJS__QUOTED += \
 "main.obj" \
-"tm4c123gh6pm_startup_ccs.obj" \
-"uartstdio.obj" 
+"tm4c123gh6pm_startup_ccs.obj" 
 
 C_SRCS__QUOTED += \
 "../main.c" \
-"../tm4c123gh6pm_startup_ccs.c" \
-"C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.c" 
+"../tm4c123gh6pm_startup_ccs.c" 
 
 
--- code/filter/filter/main.c
+++ code/filter/filter/main.c
@@ -1,9 +1,13 @@
 //*****************************************************************************
 //
-// Filter 1
-//
+// Filter 2
+// IIR filter with SOS structure
+// by Danilo Pena
 
-#define N 100
+#define N 1000
+#define T 120
+#define SECTIONS 20
+#define DELAY 2
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -13,34 +17,36 @@
 #include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
+#include "driverlib/adc.h"
 #include "utils/uartstdio.h"
 #include <math.h>
 
+unsigned char bytes[4*T];
+
+float vf[T];
+
+float sinal[N];
+float aux[N];
 float x[N];
 float y[N];
-float xn, yn, yn2;
-char tmp;
-float tmp2;
-char tmp3[9];
-bool menos;
-bool ponto;
 
-float vf[100];
-unsigned char vb[400];
-
-void ftoa(float f,char *buf);
+float a0[SECTIONS];
+float a1[SECTIONS];
+float a2[SECTIONS];
+float b0[SECTIONS];
+float b1[SECTIONS];
+float b2[SECTIONS];
+float w[SECTIONS][3] = {0};
 
 int
 main(void)
 {
-	int n=0, i=0, j=0;
+	int n=0, m=0, i=0, s=0;
+	uint32_t pui32ADC0Value[1];
 
-	y[0] = 0;
-	y[1] = 0;
-	y[2] = 0;
-	y[3] = 0;
-
-	SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+	/*SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+			SYSCTL_XTAL_16MHZ);*/
+	SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
 			SYSCTL_XTAL_16MHZ);
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
@@ -51,35 +57,73 @@ main(void)
 
 	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 9600,
+	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
 			(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
 					UART_CONFIG_PAR_NONE));
 
-	//UARTStdioConfig(0, 115200, 16000000);
+	//ADC
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+	GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
+	ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+	ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
+			ADC_CTL_END);
+	ADCSequenceEnable(ADC0_BASE, 3);
+	ADCIntClear(ADC0_BASE, 3);
 
 	do
 	{
-		vb[n] = UARTCharGet(UART0_BASE);
+		ADCProcessorTrigger(ADC0_BASE, 3);
+		while(!ADCIntStatus(ADC0_BASE, 3, false)){}
 
+		ADCIntClear(ADC0_BASE, 3);
+		ADCSequenceDataGet(ADC0_BASE, 3, pui32ADC0Value);
+
+		//UARTprintf("AIN0 = %4d\r", pui32ADC0Value[0]);
+		sinal[m] = (float) pui32ADC0Value[0];
+
+//		SysCtlDelay(SysCtlClockGet() / 12);
+
+		m++;
+	}
+	while(m<(N));
+
+	do
+	{
+		bytes[n] = UARTCharGet(UART0_BASE);
 		n++;
 	}
-	while(n<400);
+	while(n<(4*T));
 
-	memcpy(vf, vb, sizeof(unsigned char)*400);
+	memcpy(vf, bytes, sizeof(unsigned char)*4*T);
 
-	for(j=0;j<100;j++){
-		if(n > 3){
-			for(i=0;i<4;i++){
-				y[j] = y[j] + vf[j - i]*(0.25);
-			}
+	memcpy(a0, vf, sizeof(float)*SECTIONS);
+	memcpy(a1, vf+SECTIONS, sizeof(float)*SECTIONS);
+	memcpy(a2, vf+2*SECTIONS, sizeof(float)*SECTIONS);
+	memcpy(b0, vf+3*SECTIONS, sizeof(float)*SECTIONS);
+	memcpy(b1, vf+4*SECTIONS, sizeof(float)*SECTIONS);
+	memcpy(b2, vf+5*SECTIONS, sizeof(float)*SECTIONS);
+
+	memcpy(x, sinal, sizeof(float)*N);
+
+	// Filter direct-form II SOS
+	memcpy(aux, x, sizeof(unsigned char)*4*N);
+
+	for(i=2; i<(N+2); i++){
+		for(s=0; s<SECTIONS; s++){
+			w[s][2] = a0[s]*aux[i-2] - a1[s]*w[s][1] - a2[s]*w[s][0];
+			aux[i-2] = b0[s]*w[s][2] + b1[s]*w[s][1] + b2[s]*w[s][0];
+			w[s][0] = w[s][1];
+			w[s][1] = w[s][2];
 		}
+		y[i-2] = aux[i-2];
 	}
 
-	memcpy(vb, y, sizeof(float)*100);
+	memcpy(bytes, y, sizeof(float)*N);
 
 	for(i=0;i<n;i++){
 		//SysCtlDelay(5000);
-		UARTCharPut(UART0_BASE, vb[i]);
+		UARTCharPut(UART0_BASE, bytes[i]);
 	}
 
 	return(0);
